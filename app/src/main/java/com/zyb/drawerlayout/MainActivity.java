1fc47be5eed903fd8ef0229220b21031f6ff6e0a
zhangyb <13619876055@163.com>
2017-12-12 21:38:11 +0800

    add listview

--- app/src/main/java/com/zyb/drawerlayout/MainActivity.java
+++ app/src/main/java/com/zyb/drawerlayout/MainActivity.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,31 +21,26 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //设置导航图标
         toolbar.setNavigationIcon(R.mipmap.ic_launcher_round);
 
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.action_settings:
-                        Toast.makeText(MainActivity.this, "setting 1", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.action_settings1:
-                        Toast.makeText(MainActivity.this, "setting 2", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.action_settings2:
-                        Toast.makeText(MainActivity.this, "setting 3", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-                return true;
-            }
-        });
+        listView = (ListView) findViewById(R.id.left_drawer);
+        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, getData()));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private List<String> getData() {
+
+        List<String> data = new ArrayList<>();
+        data.add("测试数据1");
+        data.add("测试数据2");
+        data.add("测试数据3");
+        data.add("测试数据4");
+
+        return data;
     }
 }
