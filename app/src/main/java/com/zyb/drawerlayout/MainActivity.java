d11ac33dd1bf3277b3595b7a68af33dc1a2b5496
zhangyb <13619876055@163.com>
2017-12-12 21:53:35 +0800

    add click toolbar, open drawer layout

--- app/src/main/java/com/zyb/drawerlayout/MainActivity.java
+++ app/src/main/java/com/zyb/drawerlayout/MainActivity.java
@@ -1,9 +1,13 @@
 package com.zyb.drawerlayout;
 
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,6 +16,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
     private ListView listView;
 
     @Override
@@ -19,9 +24,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.mipmap.ic_launcher_round);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
 
         listView = (ListView) findViewById(R.id.left_drawer);
         listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, getData()));
@@ -34,8 +48,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private List<String> getData() {
-
         List<String> data = new ArrayList<>();
+
         data.add("测试数据1");
         data.add("测试数据2");
         data.add("测试数据3");
