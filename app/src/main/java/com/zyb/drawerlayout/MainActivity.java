87ea5930e44752be21a408f01fc88c42c472beea
smith <zhangyb@fsmeeting.com>
2017-12-12 18:02:29 +0800

    drawerlayout

---
+++ app/src/main/java/com/zyb/drawerlayout/MainActivity.java
@@ -0,0 +1,46 @@
+package com.zyb.drawerlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //设置导航图标
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher_round);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_settings:
+                        Toast.makeText(MainActivity.this, "setting 1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_settings1:
+                        Toast.makeText(MainActivity.this, "setting 2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_settings2:
+                        Toast.makeText(MainActivity.this, "setting 3", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
